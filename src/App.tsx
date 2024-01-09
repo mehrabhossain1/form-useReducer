45143bc6256217a9fc400eeb7664b0c737db1571
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-09 22:17:54 +0600

    form using useReducer

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import UseReducerForm from './components/UseReducerForm'
+
+function App() {
+  return (
+    <>
+      <UseReducerForm />
+    </>
+  )
+}
+
+export default App
